ce59db3b693b983d50b40b25221e9f7aa07ca561
Giovanni Lovato <heruan@aldu.net>
2016-05-09 11:43:05 +0200

    fix(decorators): fix decorators signatures for TypeScript projects

--- config.js
+++ config.js
@@ -7,19 +7,19 @@ System.config({
   },
 
   map: {
-    "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.2.0",
+    "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.2.2",
     "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.2.0",
-    "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.1.2",
-    "npm:aurelia-dependency-injection@1.0.0-beta.1.2.0": {
+    "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.1.3",
+    "npm:aurelia-dependency-injection@1.0.0-beta.1.2.2": {
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.2.0",
       "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.2.0",
-      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.0"
+      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.1"
     },
     "npm:aurelia-metadata@1.0.0-beta.1.2.0": {
-      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.0"
+      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.1"
     },
-    "npm:aurelia-polyfills@1.0.0-beta.1.1.2": {
-      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.0"
+    "npm:aurelia-polyfills@1.0.0-beta.1.1.3": {
+      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.1"
     }
   }
 });
--- src/decorators.js
+++ src/decorators.js
@@ -4,7 +4,7 @@ import { All, Parent, Lazy, Optional, Factory } from 'aurelia-dependency-injecti
 /**
 * Decorator: Directs the TypeScript transpiler to write-out type metadata for the decorated class/property.
 */
-export function autoinject(potentialTarget, potentialKey) {
+export function autoinject(potentialTarget?: any, potentialKey?: any): any {
     let deco = function (target, key, descriptor) {
         if (key === undefined) {
             target.inject = metadata.getOwn(metadata.paramTypes, target, key) || Object.freeze([]);
@@ -22,7 +22,7 @@ export function autoinject(potentialTarget, potentialKey) {
 /**
 * Decorator: Specifies the dependencies that should be injected by the DI Container into the decorated class/function/property.
 */
-export function inject(...rest) {
+export function inject(...rest: any[]): any {
     return function (target, key, descriptor) {
         if (key !== undefined) {
             if (descriptor.configurable) {
