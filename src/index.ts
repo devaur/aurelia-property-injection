44b35be289aacd1524dd8332ccdae0884a369ca4
Matjaz Lipus <matjazl@gmail.com>
2016-06-25 23:18:26 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { InvocationHandlerWrapper } from "./invocation-handler-wrapper";
+import { InvocationHandlerWrapper } from './invocation-handler-wrapper';
 
 export function configure(frameworkConfiguration) {
-    frameworkConfiguration.container.setHandlerCreatedCallback((handler) => {
-        return new InvocationHandlerWrapper(handler.fn, handler.invoker, handler.dependencies);
-    });
+    frameworkConfiguration.container.setHandlerCreatedCallback(handler =>
+        new InvocationHandlerWrapper(handler.fn, handler.invoker, handler.dependencies)
+    );
 }
 
 export {InvocationHandlerWrapper};
